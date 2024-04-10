2b7ba35977139b61b038b623f652d8da711a85c7
Andrea Falconi <andrea.falconi@gmail.com>
2024-04-10 16:19:17 +0200

    note down new ec2 instance name.

--- nix/nodes/ec2-aarch64/configuration.nix
+++ nix/nodes/ec2-aarch64/configuration.nix
@@ -1,7 +1,7 @@
 #
-# NixOS config to define the EC2 VM:
-# - public FQDN: ec2-54-76-182-114.eu-west-1.compute.amazonaws.com
-# - hostname: ip-172-31-3-69.eu-west-1.compute.internal
+# NixOS config to define the on-demand EC2 Graviton VM (c6g.xlarge):
+# - public FQDN: ec2-54-194-164-145.eu-west-1.compute.amazonaws.com
+# - hostname: ip-172-31-13-25.eu-west-1.compute.internal
 #
 # Notice this is the main config with the full Odoo service stack.
 #
@@ -10,6 +10,7 @@
   imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
   ec2.efi = true;
 
+  # hostname automatically set, so we leave it be.
   time.timeZone = "Europe/Amsterdam";
   system.stateVersion = "23.11";
 
