b517962db6fb4f8b678cba03e5a7efee854d0d13
Andrea Falconi <andrea.falconi@gmail.com>
2024-04-15 10:11:03 +0200

    document snake oil sec pkg.

--- nix/pkgs/snakeoil-sec/pkg.nix
+++ nix/pkgs/snakeoil-sec/pkg.nix
@@ -1,19 +1,5 @@
 #
-# Snake oil security for the careless soul.
-#
-# WARNING: only ever use this package for testing with a local VM!
-#
-# In the box:
-# - Localhost cert. Self-signed, 100-year valid SSL cert for testing
-#   on localhost.
-# - Passwords. Root and admin sys users as well as Odoo admin user
-#   get a generated password of 'abc123'.
-# - SSH keys. ED25519 identity and corresponding public key.
-#
-# We use `vaultgen` to generate all the above and put the content
-# of the `generated` dir in this package's root dir. See `vaultgen`
-# for the details of the directory structure and what files get
-# generated.
+# See `docs.md` for package documentation.
 #
 {
   stdenv, vaultgen
@@ -59,14 +45,4 @@ in stdenv.mkDerivation {
 # warning when hitting localhost:443.
 # See:
 # - https://hackernoon.com/how-to-get-sslhttps-for-localhost-i11s3342
-#
-# 4. Decrypting age files. Easy:
-#
-#   $ cd odoo.box/nix
-#   $ nix shell
-#   $ nix build .#snakeoil-sec
-#   $ age -d -i result/age.key result/passwords/odoo-admin.age
-#   $ age -d -i result/age.key result/certs/localhost-key.pem.age | bat
-#
-# ...and so on.
 #
