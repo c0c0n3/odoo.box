9432d128fa702b282168dbacaee442aa614c7277
Andrea Falconi <andrea.falconi@gmail.com>
2024-05-03 19:47:43 +0200

    update ec2 bootstrap docs.

--- nix/nodes/ec2-aarch64/configuration.nix
+++ nix/nodes/ec2-aarch64/configuration.nix
@@ -32,7 +32,7 @@ in {
       };
     };
     vault = {
-      agez.enable = true;
+      agez.enable = true;                                      # (4)
       age = {
         odoo-admin-pwd = ./vault/passwords/odoo-admin.age;
         pgadmin-admin-pwd = ./vault/passwords/pgadmin-admin.age;
@@ -57,17 +57,25 @@ in {
 # 1. SSH pub key. Copied from the one the NixOS AMI sets up from the EC2
 # meta it fetches on boot. File:
 # - /etc/ec2-metadata/public-keys-0-openssh-key
+#
 # 2. Hostname. The `amazon-image` module automatically sets the hostname
 # from dynamically retrieved EC2 metadata, so we leave it be. Those EC2
 # hostnames all follow the same pattern:
 #     <addr>.<region>.compute.internal
 # e.g. `ip-172-31-3-61.eu-west-1.compute.internal`.
+#
 # 3. Backup disk. We use GPT partition names to reliably find the backup
 # disk. This works because we can control what those names are and we can
 # keep them unique across disks and partitions. The advantage of using a
 # GTP partition name over a UUID is that if we replace the disk with a
 # new one or migrate to another VM, our Nix config won't need to change,
-# provided we always set up a GTP partition named `backup`. See:
+# provided we always set up a GTP partition named `backup`.
+# See:
 # - Our EC2 bootstrap procedure
 # - https://wiki.archlinux.org/title/persistent_block_device_naming
 #
+# 4. Age key. Don't forget to upload it to `/etc/` the first time you
+# deploy or if you ever generate a new one.
+# See:
+# - Our EC2 bootstrap procedure
+# - Our docs about Age secrets
