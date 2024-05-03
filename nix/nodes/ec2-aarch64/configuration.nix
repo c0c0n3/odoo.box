301a7d9f04dd7598e0a6c91ecd9d0088260a3b53
Andrea Falconi <andrea.falconi@gmail.com>
2024-05-03 18:55:56 +0200

    enable backups on ec2 vm.

--- nix/nodes/ec2-aarch64/configuration.nix
+++ nix/nodes/ec2-aarch64/configuration.nix
@@ -6,20 +6,31 @@
 # Notice this is the main config with the full Odoo service stack.
 #
 { config, modulesPath, pkgs, ... }:
-{
+let
+  backup-mount-dir = "/backup";
+in {
   imports = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
   ec2.efi = true;
 
-  # The `amazon-image` module automatically sets the hostname from
-  # dynamically retrieved EC2 metadata, so we leave it be. Those
-  # EC2 hostnames all follow the same pattern:
-  #     <addr>.<region>.compute.internal
-  # e.g. `ip-172-31-3-61.eu-west-1.compute.internal`.
   time.timeZone = "Europe/Amsterdam";
   system.stateVersion = "23.11";
 
+  fileSystems."${backup-mount-dir}" = {
+    device = "/dev/disk/by-partlabel/backup";                  # (3)
+    fsType = "ext4";
+  };
+
   odbox = {
     server.enable = true;
+    backup = {
+      basedir = backup-mount-dir;
+      odoo = {
+        enable = true;
+        # hot-schedule = [ "11:00:00" "14:00:00" "16:00:00" ];
+        cold-schedule = [ "18:51:00" ];
+        # cold-schedule = [ "02:00:00" ];
+      };
+    };
     vault = {
       agez.enable = true;
       age = {
@@ -46,4 +57,17 @@
 # 1. SSH pub key. Copied from the one the NixOS AMI sets up from the EC2
 # meta it fetches on boot. File:
 # - /etc/ec2-metadata/public-keys-0-openssh-key
+# 2. Hostname. The `amazon-image` module automatically sets the hostname
+# from dynamically retrieved EC2 metadata, so we leave it be. Those EC2
+# hostnames all follow the same pattern:
+#     <addr>.<region>.compute.internal
+# e.g. `ip-172-31-3-61.eu-west-1.compute.internal`.
+# 3. Backup disk. We use GPT partition names to reliably find the backup
+# disk. This works because we can control what those names are and we can
+# keep them unique across disks and partitions. The advantage of using a
+# GTP partition name over a UUID is that if we replace the disk with a
+# new one or migrate to another VM, our Nix config won't need to change,
+# provided we always set up a GTP partition named `backup`. See:
+# - Our EC2 bootstrap procedure
+# - https://wiki.archlinux.org/title/persistent_block_device_naming
 #
